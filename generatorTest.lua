4a2fe2eb47e44c3b5835f7dd51915a5985bcd0ab
ozkriff <ozkriff@gmail.com>
2013-03-24 10:56:16 +0400

    generatorTest.lua: Added testFunc1

--- generatorTest.lua
+++ generatorTest.lua
@@ -32,6 +32,36 @@ suite.testSimple = function()
   Assert.isEqual(codeInAnsiC, expectedCodeInAnsiC)
 end
 
+suite.testFunc1 = function()
+  local ast = {
+    {
+      tag = 'functionDeclaration',
+      name = 'x1',
+      parameters = {},
+      returnValue = { {type = 'Int'} },
+      body = {
+        {
+          tag = 'variableDeclaration',
+          name = 'var1',
+          type = 'Int',
+        }
+      }
+    }
+  }
+  local codeInAnsiC = Generator.generate(ast)
+  local expectedCodeInAnsiC =
+      '#include <stdio.h>\n' ..
+      '\n' ..
+      'typedef int Int;\n' ..
+      'typedef float Float;\n' ..
+      '\n' ..
+      'Int x1() {\n' ..
+      '  Int var1;\n' ..
+      '}\n' ..
+      '\n'
+  Assert.isEqual(codeInAnsiC, expectedCodeInAnsiC)
+end
+
 -- TODO: write to some file
 -- local outFile = io.open('out.c', 'w')
 -- outFile:write(codeInAnsiC)
